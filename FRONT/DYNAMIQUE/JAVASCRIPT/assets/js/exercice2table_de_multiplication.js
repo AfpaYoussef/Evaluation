8408bc59016c559f65ca1aa9ec76b35af7d31dcc
AfpaYoussef <youssefchaibi@hotmail.com>
2022-10-28 12:21:18 +0000

    Nouveau dépot

---
+++ FRONT/DYNAMIQUE/JAVASCRIPT/assets/js/exercice2table_de_multiplication.js
@@ -0,0 +1,49 @@
+const number = parseInt(prompt('Enter an integer: '));
+
+for(let i = 1; i <= 10; i++) {
+
+    const result = i * number;
+
+    console.log(`${number} * ${i} = ${result}`);
+}
+
+
+
+
+
+
+
+
+
+// solution 2
+
+// Variables
+// var tablemult = parseInt(prompt("Entrez l'entier pour lequel vous souhaitez afficher la table de multiplication:"));
+
+
+// // Fonction
+// for(var j=1;j<=10;j++){
+//     document.write("<h4>La table de multiplication du nombre: "+j+"</h4>")
+//  document.write("<table border >");
+//     for(var i=1;i<=10;i++){
+//      document.write("<tr>");
+//      document.write("<td>"+j+" x "+i+" =</td>");
+//      document.write("<td>"+j*i+"</td>");
+//      document.write("</tr>");
+// }
+// document.write("</table>");
+// }
+
+
+
+
+// solution 1  
+
+
+// nombre=window.prompt("Entrez un entier:");
+// var b =window.prompt("Entrez le nombre de multiples:");
+
+
+// for (i = 1; i <= b; i++) {
+// document.write(nombre+ " x " +i+ " = "+nombre*i+"<br>");
+// } 
